1de6c83d2042ed059b19668ed2ea81663e7d1f23
stevenhiltrop <stevenhiltrop@gmail.com>
2021-12-25 18:47:22 +0700

    Added request, response structs and handler function

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+type Request struct {
+	ID    float64 `json:"id"`
+	Value string  `json:"value"`
+}
+
+type Response struct {
+	Message string `json:"message"`
+	OK      bool   `json:"ok"`
+}
+
+func Handler(request Request) (Response, error) {
+	return Response{
+		Message: fmt.Sprintf("Process Request ID %f", request.ID),
+		OK:      true,
+	}, nil
 }
